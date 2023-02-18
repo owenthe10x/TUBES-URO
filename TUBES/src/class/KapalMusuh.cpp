ccc62f3dbec83611bf7eefd6ff0db079250b202e
Edia Zaki <13521141@mahasiswa.itb.ac.id>
2023-02-18 16:20:20 +0700

    feat: Empty template files

---
+++ TUBES/src/class/KapalMusuh.cpp
@@ -0,0 +1,1 @@
+#include "KapalMusuh.hpp"
---
+++ TUBES/src/main.cpp
@@ -0,0 +1,5 @@
+#include "../class/Kapal.cpp"
+#include "../class/KapalPlayer.cpp"
+#include "../class/KapalMusuh.cpp"
+#include "../class/Map.cpp"
+#include <iostream>
